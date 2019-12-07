d25c9dd89f7a34180b102d1f438b3301b72567c7
Blake Shurtz <blakeobeans@gmail.com>
2019-12-07 11:29:41 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,9 +9,9 @@ from fastai import *
 # from fastai.vision import *
 from fastai.text import *
 
-export_file_url = 'https://www.dropbox.com/s/xhnvw0axn6xjbk9/export.pkl?dl=1'
-# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/1-3TEgcbSePWaDVxxeCfKMxYnvAE7Jwru/view?usp=sharing'
+# export_file_url = 'https://drive.google.com/file/d/1-3TEgcbSePWaDVxxeCfKMxYnvAE7Jwru/view?usp=sharing'
+export_file_name = 'classifier_model.pkl'
 # added comment
 classes = ['neg', 'pos']
 path = Path(__file__).parent
